f54e3ae340eb473722aa6bd3165d48356de8990f
Vladislav <tsekov.vladislav@gmail.com>
2024-03-09 11:51:29 +0200

    Scraper will target selected nodes as intended.

--- DirtX.Scraper/Scraper.cs
+++ DirtX.Scraper/Scraper.cs
@@ -9,6 +9,8 @@ namespace DirtX.Scraper
     {
         public async Task Run(string category)
         {
+            Config();
+
             List<string> makes = new();
             List<int> displacements = new();
             List<decimal> prices = new();
@@ -16,8 +18,8 @@ namespace DirtX.Scraper
             List<string> links = new();
 
             using HttpClient client = new();
-            string baseUrl = string.Empty;
 
+            string baseUrl = string.Empty;
             if (category.ToLower() == "motocross")
             {
                 baseUrl = MxBaseUrl;
@@ -25,6 +27,37 @@ namespace DirtX.Scraper
             else
             {
                 baseUrl = EnduroBaseUrl;
+            }
+
+            int maxPages = 150;
+            int doomCounter = 0;
+
+            for (int i = 1; i <= maxPages; i++)
+            {
+                if (doomCounter > 1)
+                {
+                    break;
+                }
+
+                string currentPage = baseUrl + i;
+
+                HttpResponseMessage httpResponse = await client.GetAsync(currentPage);
+
+                if (httpResponse.IsSuccessStatusCode)
+                {
+                    Stream contentStream = await httpResponse.Content.ReadAsStreamAsync();
+
+                    using StreamReader reader = new(contentStream, Encoding.GetEncoding("windows-1251"));
+                    string htmlContent = await reader.ReadToEndAsync();
+                    HtmlDocument doc = new();
+                    doc.LoadHtml(htmlContent);
+
+                    HtmlNodeCollection titleNodes = doc.DocumentNode.SelectNodes(TitleNodes);
+                    HtmlNodeCollection priceNodes = doc.DocumentNode.SelectNodes(PriceNodes);
+                    HtmlNodeCollection descriptionNodes = doc.DocumentNode.SelectNodes(DescriptionNodes);
+                    HtmlNodeCollection linkNodes = doc.DocumentNode.SelectNodes(LinkNodes);
+
+                }
             }
         }
     }
--- DirtX.Scraper/Settings.cs
+++ DirtX.Scraper/Settings.cs
@@ -11,5 +11,10 @@ namespace DirtX.Scraper
 
         public const string MxBaseUrl = "https://www.mobile.bg/obiavi/motori/krosov/ot-2006/do-2024/p-";
         public const string EnduroBaseUrl = "";
+
+        public const string TitleNodes = "//a[@class='mmmL']";
+        public const string PriceNodes = "//span[@class='price']";
+        public const string DescriptionNodes = "//td[(contains(@colspan,'3') or contains(@colspan,'4')) and contains(@style,'padding-left:')]";
+        public const string LinkNodes = "//a[@class='photoLinkL']";
     }
 }

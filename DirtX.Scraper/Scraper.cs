e19f51563c4cea880b2836453e79f153a5c615db
Vladislav <tsekov.vladislav@gmail.com>
2024-03-09 11:44:43 +0200

    Started the implementation of a web scraper.

---
+++ DirtX.Scraper/Scraper.cs
@@ -0,0 +1,31 @@
+﻿using HtmlAgilityPack;
+using System.Text;
+using System.Text.RegularExpressions;
+using static DirtX.Scraper.Settings;
+
+namespace DirtX.Scraper
+{
+    public class Scraper
+    {
+        public async Task Run(string category)
+        {
+            List<string> makes = new();
+            List<int> displacements = new();
+            List<decimal> prices = new();
+            List<int> years = new();
+            List<string> links = new();
+
+            using HttpClient client = new();
+            string baseUrl = string.Empty;
+
+            if (category.ToLower() == "motocross")
+            {
+                baseUrl = MxBaseUrl;
+            }
+            else
+            {
+                baseUrl = EnduroBaseUrl;
+            }
+        }
+    }
+}
---
+++ DirtX.Scraper/Settings.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+namespace DirtX.Scraper
+{
+    public static class Settings
+    {
+        public static void Config() 
+        { 
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance); 
+        }
+
+        public const string MxBaseUrl = "https://www.mobile.bg/obiavi/motori/krosov/ot-2006/do-2024/p-";
+        public const string EnduroBaseUrl = "";
+    }
+}

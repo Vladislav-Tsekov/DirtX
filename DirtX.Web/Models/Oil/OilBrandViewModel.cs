a499a0841fbe4b8b9b912d32159f94e87644bf58
Vladislav <tsekov.vladislav@gmail.com>
2024-03-12 18:02:17 +0200

    Implemented View and Model for Oil\Brand.

---
+++ DirtX.Web/Models/Oil/OilBrandViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DirtX.Models.Part;
+using DirtX.Infrastructure.Data.Models.ProductModels;
+
+public class OilBrandViewModel
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+    public List<Oil> Oils { get; set; }
+}

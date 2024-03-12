65663c7be09bbd63068324c93622714c3f18a319
Vladislav <tsekov.vladislav@gmail.com>
2024-03-12 20:27:39 +0200

    Implemented View and Model for Gear\Index.

---
+++ DirtX.Web/Models/Gear/GearIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DirtX.Models.Gear;
+using DirtX.Infrastructure.Data.Models.ProductModels;
+
+public class GearIndexViewModel
+{
+    public string CategoryName { get; set; }
+    public string ImageUrl { get; set; }
+    public List<ProductBrand> Brands { get; set; }
+}

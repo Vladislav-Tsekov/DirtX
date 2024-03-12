fdf834c11710f9aae413c3b83e51f6c5eb3ed9dc
Vladislav <tsekov.vladislav@gmail.com>
2024-03-12 20:27:31 +0200

    Implemented View and Model for Gear\Details.

---
+++ DirtX.Web/Models/Gear/GearDetailsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using DirtX.Infrastructure.Data.Models.Enums;
+
+namespace DirtX.Models.Gear
+{
+    public class GearDetailsViewModel
+    {
+        public int Id { get; set; }
+        public GearType Type { get; set; }
+        public string BrandName { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public bool IsAvailable { get; set; }
+        public int StockQuantity { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}

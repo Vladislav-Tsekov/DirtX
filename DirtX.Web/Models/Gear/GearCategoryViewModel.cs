b75b91e4636d46aa123c6856f77f6a7538b5719f
Vladislav <tsekov.vladislav@gmail.com>
2024-03-12 20:27:17 +0200

    Implemented View and Model for Gear\Category.

---
+++ DirtX.Web/Models/Gear/GearCategoryViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DirtX.Models.Gear;
+using DirtX.Infrastructure.Data.Models.ProductModels;
+
+public class GearCategoryViewModel
+{
+    public string CategoryName { get; set; }
+    public List<Gear> Gears { get; set; }
+}

a4676bc02b3084fe0f1d11d2d121d376632aacae
Vladislav <tsekov.vladislav@gmail.com>
2024-03-14 15:03:56 +0200

    Prepared some MVC templates for the Used Motorcycles page.

---
+++ DirtX.Web/Controllers/UsedController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DirtX.Controllers
+{
+    public class UsedController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Sell()
+        {
+            return View();
+        }
+    }
+}

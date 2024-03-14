95462004b44d1ae5c26788b05b3f09ffb15d781d
Vladislav <tsekov.vladislav@gmail.com>
2024-03-14 21:42:57 +0200

    Implemented the Index action and view for the Used Ctrl.

--- DirtX.Web/Controllers/UsedController.cs
+++ DirtX.Web/Controllers/UsedController.cs
@@ -1,12 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DirtX.Web.Data;
+using DirtX.Web.Models.Used;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-namespace DirtX.Controllers
+namespace DirtX.Web.Controllers
 {
     public class UsedController : Controller
     {
-        public IActionResult Index()
+        //private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext context;
+
+        public UsedController(ApplicationDbContext _context/*ILogger<HomeController> logger*/)
         {
-            return View();
+            //_logger = logger;
+            context = _context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var usedMotorcycles = await context.UsedMotorcycles
+                .Include(um => um.Make)
+                .Include(um => um.Model)
+                .Include(um => um.Year)
+                .Include(um => um.Displacement)
+                .ToListAsync();
+
+            List<UsedIndexViewModel> models = usedMotorcycles.Select(m => new UsedIndexViewModel
+            {
+                Id = m.Id,
+                Make = m.Make.Title,
+                Model = m.Model.Title,
+                Displacement = m.Displacement.Volume,
+                Year = m.Year.ManufactureYear,
+                Image = m.Image,
+                Province = m.Province.ToString(),
+                Description = m.Description,
+                Price = m.Price,
+                Contact = m.Contact
+            }).ToList();
+
+            return View(models);
         }
 
         public IActionResult Sell()
--- DirtX.Web/Models/Used/UsedIndexViewModel.cs
+++ DirtX.Web/Models/Used/UsedIndexViewModel.cs
@@ -1,17 +1,16 @@
-﻿using DirtX.Infrastructure.Data.Models.Enums;
+﻿namespace DirtX.Web.Models.Used;
+using DirtX.Infrastructure.Data.Models.Enums;
 
-namespace DirtX.Models.Used
+public class UsedIndexViewModel
 {
-    public class UsedIndexViewModel
-    {
-        public string Make { get; set; }
-        public string Model { get; set; }
-        public int Displacement { get; set; }
-        public int Year { get; set; }
-        public byte[] Image { get; set; }
-        public Province Province { get; set; }
-        public string Description { get; set; }
-        public int Price { get; set; }
-        public string Contact { get; set; }
-    }
+    public int Id { get; set; }
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Displacement { get; set; }
+    public int Year { get; set; }
+    public byte[] Image { get; set; }
+    public string Province { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+    public string Contact { get; set; }
 }

3edbbdd3a10c78ee9d8cdda62eae7eed700357a0
Vladislav <tsekov.vladislav@gmail.com>
2024-03-17 18:47:23 +0200

    Created a common "ProductBrand" view model to avoid repetition.

---
+++ DirtX.Core/Models/ProductBrandViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DirtX.Core.Models
+{
+    public class ProductBrandViewModel<T>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public List<T> Products { get; set; }
+    }
+}

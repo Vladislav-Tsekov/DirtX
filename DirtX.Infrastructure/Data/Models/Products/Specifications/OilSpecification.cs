f84a36d9e655c10acf93e4851e3880bcc6597a04
Vladislav <tsekov.vladislav@gmail.com>
2024-03-17 21:00:01 +0200

    Specifications and properties were deemed useless and removed.

--- DirtX.Infrastructure/Data/Models/Products/Specifications/OilSpecification.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DirtX.Infrastructure.Data.Models.Products.Properties
-{
-    public class OilSpecification : ProductSpecification
-    {
-        public List<OilProperty> OilProperties { get; set; }
-    }
-}

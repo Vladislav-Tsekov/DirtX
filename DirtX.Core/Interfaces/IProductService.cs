fddeacb5475a81826d8b7b126bc2c30e32ee0ad6
Vladislav <tsekov.vladislav@gmail.com>
2024-03-17 15:49:45 +0200

    Implemented an interface IProductService.

---
+++ DirtX.Core/Interfaces/IProductService.cs
@@ -0,0 +1,10 @@
+﻿namespace DirtX.Core.Interfaces
+{
+    public interface IProductService<T>
+    {
+        Task<List<T>> GetByBrandAsync(string brandName);
+        Task<List<T>> GetAllByCategoryAsync(Enum category);
+        Task<List<T>> GetByCategoryAsync(Enum category);
+        Task<T> GetDetailsAsync(int id);
+    }
+}

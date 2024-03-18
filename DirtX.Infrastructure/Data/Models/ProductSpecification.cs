c99271abcddd53be4a965edf42a5181f84488c30
Vladislav <tsekov.vladislav@gmail.com>
2024-03-18 13:25:10 +0200

    Implemented a mapping table for Specification and Product.

---
+++ DirtX.Infrastructure/Data/Models/ProductSpecification.cs
@@ -0,0 +1,16 @@
+﻿using DirtX.Infrastructure.Data.Models.Products;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DirtX.Infrastructure.Data.Models
+{
+    public class ProductSpecification
+    {
+        [ForeignKey(nameof(ProductId))]
+        public Product Product { get; set; }
+        public int ProductId { get; set; }
+
+        [ForeignKey(nameof(SpecificationId))]
+        public Specification Specification { get; set; }
+        public int SpecificationId { get; set; } 
+    }
+}

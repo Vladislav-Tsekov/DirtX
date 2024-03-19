58d1f69e1e649a4d6c6740de0e1ae7eb92fa8ac6
Vladislav <tsekov.vladislav@gmail.com>
2024-03-19 18:41:19 +0200

    Modified Trailers entities, but development on them is not finished.

--- DirtX.Infrastructure/Data/Models/Trailers/TrailerRent.cs
+++ DirtX.Infrastructure/Data/Models/Trailers/TrailerRent.cs
@@ -1,12 +1,28 @@
-﻿namespace DirtX.Infrastructure.Data.Models.Trailers
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DirtX.Infrastructure.Data.Models.Trailers
 {
     public class TrailerRent
     {
+        [Key]
         public int RentalId { get; set; }
+
+        [ForeignKey(nameof(TrailerId))]
+        public Trailer Trailer { get; set; }
         public int TrailerId { get; set; }
-        public int UserId { get; set; }
+
+        //TODO - FOREIGN KEY TO USER
+        //public string UserId { get; set; }
+
+        [Required]
         public DateTime StartDate { get; set; }
+
+        [Required]
         public DateTime ReturnDate { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10, 2)")]
         public decimal TotalCost { get; set; }
     }
 }

19200c6a1339657948a6e68d551d594fccef0d3a
Vladislav <tsekov.vladislav@gmail.com>
2024-03-19 11:19:10 +0200

    Implemented Trailer and TrailerRent entities.

---
+++ DirtX.Infrastructure/Data/Models/Trailers/Trailer.cs
@@ -0,0 +1,12 @@
+﻿namespace DirtX.Infrastructure.Data.Models.Trailers
+{
+    public class Trailer
+    {
+        public int TrailerId { get; set; }
+        public string TrailerType { get; set; }
+        public decimal CostPerDay { get; set; }
+        public int Capacity { get; set; }
+        public decimal MaximumLoad { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
---
+++ DirtX.Infrastructure/Data/Models/Trailers/TrailerRent.cs
@@ -0,0 +1,12 @@
+﻿namespace DirtX.Infrastructure.Data.Models.Trailers
+{
+    public class TrailerRent
+    {
+        public int RentalId { get; set; }
+        public int TrailerId { get; set; }
+        public int UserId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public decimal TotalCost { get; set; }
+    }
+}

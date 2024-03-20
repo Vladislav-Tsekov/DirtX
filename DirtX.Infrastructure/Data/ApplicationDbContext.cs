56c70ded53ca71b432594ede10a8f2a6c2e509c2
Vladislav <tsekov.vladislav@gmail.com>
2024-03-20 19:14:35 +0200

    Updated the Fluent API accordingly.

--- DirtX.Infrastructure/Data/ApplicationDbContext.cs
+++ DirtX.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using DirtX.Infrastructure.Data.Models;
 using DirtX.Infrastructure.Data.Models.Motorcycles;
+using DirtX.Infrastructure.Data.Models.Orders;
 using DirtX.Infrastructure.Data.Models.Products;
 using DirtX.Infrastructure.Data.Models.Trailers;
 using DirtX.Infrastructure.Data.Seeders;
@@ -34,10 +35,14 @@ namespace DirtX.Web.Data
         public DbSet<TrailerRent> TrailersRents { get; set; }
 
         //TODO - ADD ORDER RELATED ENTITIES
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Wishlist> Wishlists { get; set; }
 
         // MAPPING/JUNCTION TABLES
         public DbSet<MotorcyclePart> MotorcyclesParts { get; set; }
         public DbSet<ProductSpecification> ProductsSpecifications { get; set; }
+        public DbSet<CartProduct> CartsProducts { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {

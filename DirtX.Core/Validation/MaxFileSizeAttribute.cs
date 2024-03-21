3176cdabd905f4b9ba4f91c5b03feadf6b690548
Vladislav <tsekov.vladislav@gmail.com>
2024-03-21 22:55:23 +0200

    Added a custom validation attribute for MaxFileSize.

---
+++ DirtX.Core/Validation/MaxFileSizeAttribute.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace DirtX.Core.Validation
+{
+    public class MaxFileSizeAttribute : ValidationAttribute
+    {
+        private readonly int maxFileSize;
+
+        public MaxFileSizeAttribute(int _maxFileSize)
+        {
+            maxFileSize = _maxFileSize;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is IFormFile file)
+            {
+                if (file.Length > maxFileSize)
+                {
+                    return new ValidationResult($"The file size must not exceed {maxFileSize / 1024 / 1024}MB.");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}

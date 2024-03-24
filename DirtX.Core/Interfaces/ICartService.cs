c584a42cb2c75f4caf021b94c158a72cb3d97b78
Vladislav <tsekov.vladislav@gmail.com>
2024-03-24 22:51:06 +0200

    Implemented basic idea for ICartInterface and it's functions.

---
+++ DirtX.Core/Interfaces/ICartService.cs
@@ -0,0 +1,19 @@
+﻿namespace DirtX.Core.Interfaces
+{
+    public interface ICartService
+    {
+        //Task<CartFormViewModel?> GetCurrCartByUserIdAsync(string userId);
+
+        //Task<CartFormViewModel> GetCartByOrderIdAsync(string orderId);
+
+        Task CreateCartAsync(string userId);
+
+        Task AddItemToCartAsync(int itemId, string cartId, string userId);
+
+        Task RemoveItemFromCartAsync(int cartItemId, string cartId);
+
+        Task IncreaseItemQuantityAsync(int itemId, string cartId);
+
+        Task DecreaseItemQuantityAsync(int itemId, string cartId);
+    }
+}

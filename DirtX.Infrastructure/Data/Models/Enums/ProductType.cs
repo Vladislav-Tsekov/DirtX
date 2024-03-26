7b9d6ea9a578abe413b72d0437194aec751b100a
Vladislav <tsekov.vladislav@gmail.com>
2024-03-26 21:45:29 +0200

    Deleted separate ProductType enums and created a universal one.

---
+++ DirtX.Infrastructure/Data/Models/Enums/ProductType.cs
@@ -0,0 +1,21 @@
+﻿namespace DirtX.Infrastructure.Data.Models.Enums
+{
+    public enum ProductType
+    {
+        Engine,
+        Filters,
+        Drivetrain,
+        Braking_System,
+        Suspension,
+        Two_Stroke,
+        Four_Stroke,
+        Transmission,
+        Fork_and_Shock,
+        Coolant,
+        Helmet,
+        Protective_Gear,
+        Outfit,
+        Boots,
+        Accessory
+    }
+}

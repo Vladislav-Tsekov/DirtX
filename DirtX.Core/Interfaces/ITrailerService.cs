6575d3b19fbe0281293843f1358100f05f083342
Vladislav <tsekov.vladislav@gmail.com>
2024-04-04 21:24:12 +0300

    Added Trailer interface and service.

---
+++ DirtX.Core/Interfaces/ITrailerService.cs
@@ -0,0 +1,10 @@
+﻿using DirtX.Infrastructure.Data.Models.Trailers;
+
+namespace DirtX.Core.Interfaces
+{
+    public interface ITrailerService
+    {
+        Task<List<Trailer>> GetAllTrailersAsync();
+        Task<Trailer> GetTrailerRentsById(int id);
+    }
+}
---
+++ DirtX.Core/Services/TrailerService.cs
@@ -0,0 +1,18 @@
+﻿using DirtX.Core.Interfaces;
+using DirtX.Infrastructure.Data.Models.Trailers;
+
+namespace DirtX.Core.Services
+{
+    public class TrailerService : ITrailerService
+    {
+        public Task<List<Trailer>> GetAllTrailersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Trailer> GetTrailerRentsById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

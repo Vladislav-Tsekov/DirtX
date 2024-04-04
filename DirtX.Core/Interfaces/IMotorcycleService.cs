95ee28bf4917e3995a2207a6ad64b3740953d23c
Vladislav <tsekov.vladislav@gmail.com>
2024-04-04 21:24:02 +0300

    Added Motorcycle interface and service.

---
+++ DirtX.Core/Interfaces/IMotorcycleService.cs
@@ -0,0 +1,10 @@
+﻿using DirtX.Infrastructure.Data.Models.Motorcycles;
+
+namespace DirtX.Core.Interfaces
+{
+    public interface IMotorcycleService
+    {
+        Task<UsedMotorcycle> GetUsedMotorcycleAsync();
+        Task<List<UsedMotorcycle>> GetAllUsedMotorcyclesAsync();
+    }
+}
---
+++ DirtX.Core/Services/MotorcycleService.cs
@@ -0,0 +1,18 @@
+﻿using DirtX.Core.Interfaces;
+using DirtX.Infrastructure.Data.Models.Motorcycles;
+
+namespace DirtX.Core.Services
+{
+    public class MotorcycleService : IMotorcycleService
+    {
+        public Task<List<UsedMotorcycle>> GetAllUsedMotorcyclesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UsedMotorcycle> GetUsedMotorcycleAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

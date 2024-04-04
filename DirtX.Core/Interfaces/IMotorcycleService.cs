f484b4f63394a2f3086afc3bb828364aeb268a85
Vladislav <tsekov.vladislav@gmail.com>
2024-04-04 22:54:53 +0300

    Fully implemented DB access for MotorcycleService.

--- DirtX.Core/Interfaces/IMotorcycleService.cs
+++ DirtX.Core/Interfaces/IMotorcycleService.cs
@@ -1,10 +1,16 @@
 ﻿using DirtX.Infrastructure.Data.Models.Motorcycles;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace DirtX.Core.Interfaces
 {
     public interface IMotorcycleService
     {
-        Task<UsedMotorcycle> GetUsedMotorcycleAsync();
+        Task<int> AddUsedMotorcycleAsync(UsedMotorcycle usedMotorcycle);
+        Task<UsedMotorcycle> GetUsedMotorcycleAsync(int id);
         Task<List<UsedMotorcycle>> GetAllUsedMotorcyclesAsync();
+        Task<List<SelectListItem>> GetMotorcycleMake();
+        Task<List<SelectListItem>> GetMotorcycleModel(int makeId);
+        Task<List<SelectListItem>> GetMotorcycleDisplacement(int makeId, int modelId);
+        Task<List<SelectListItem>> GetMotorcycleYears(int makeId, int modelId, int displacementId);
     }
 }
--- DirtX.Core/Services/MotorcycleService.cs
+++ DirtX.Core/Services/MotorcycleService.cs
@@ -1,18 +1,80 @@
 ﻿using DirtX.Core.Interfaces;
+using DirtX.Infrastructure.Data;
 using DirtX.Infrastructure.Data.Models.Motorcycles;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace DirtX.Core.Services
 {
     public class MotorcycleService : IMotorcycleService
     {
-        public Task<List<UsedMotorcycle>> GetAllUsedMotorcyclesAsync()
+        private readonly ApplicationDbContext context;
+
+        public MotorcycleService(ApplicationDbContext _context)
         {
-            throw new NotImplementedException();
+            context = _context;
         }
 
-        public Task<UsedMotorcycle> GetUsedMotorcycleAsync()
+        public async Task<int> AddUsedMotorcycleAsync(UsedMotorcycle usedMotorcycle)
         {
-            throw new NotImplementedException();
+            await context.UsedMotorcycles.AddAsync(usedMotorcycle);
+            return await context.SaveChangesAsync();
+        }
+
+        public async Task<UsedMotorcycle> GetUsedMotorcycleAsync(int id)
+        {
+            return await context.UsedMotorcycles
+                .Include(um => um.Make)
+                .Include(um => um.Model)
+                .Include(um => um.Year)
+                .Include(um => um.Displacement)
+                .Where(um => um.Id == id)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<List<UsedMotorcycle>> GetAllUsedMotorcyclesAsync()
+        {
+            return await context.UsedMotorcycles
+                .AsNoTracking()
+                .Include(um => um.Make)
+                .Include(um => um.Model)
+                .Include(um => um.Year)
+                .Include(um => um.Displacement)
+                .ToListAsync();
+        }
+
+        public async Task<List<SelectListItem>> GetMotorcycleMake()
+        {
+            return await context.Makes
+                .Select(m => new SelectListItem { Value = m.Id.ToString(), Text = m.Title })
+                .ToListAsync();
+        }
+
+        public async Task<List<SelectListItem>> GetMotorcycleModel(int makeId)
+        {
+            return await context.Motorcycles
+                .Where(m => m.MakeId == makeId)
+                .Select(m => new SelectListItem { Value = m.ModelId.ToString(), Text = m.Model.Title.ToString() })
+                .Distinct()
+                .ToListAsync();
+        }
+
+        public async Task<List<SelectListItem>> GetMotorcycleDisplacement(int makeId, int modelId)
+        {
+            return await context.Motorcycles
+                .Where(m => m.MakeId == makeId && m.ModelId == modelId)
+                .Select(m => new SelectListItem { Value = m.DisplacementId.ToString(), Text = m.Displacement.Volume.ToString() })
+                .Distinct()
+                .ToListAsync();
+        }
+
+        public async Task<List<SelectListItem>> GetMotorcycleYears(int makeId, int modelId, int displacementId)
+        {
+            return await context.Motorcycles
+                .Where(m => m.MakeId == makeId && m.ModelId == modelId && m.DisplacementId == displacementId)
+                .Select(m => new SelectListItem { Value = m.YearId.ToString(), Text = m.Year.ManufactureYear.ToString() })
+                .Distinct()
+                .ToListAsync();
         }
     }
 }

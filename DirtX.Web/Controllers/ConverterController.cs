07c17ffcb5c5073ef13d34a1478435218c498400
Vladislav <tsekov.vladislav@gmail.com>
2024-04-04 18:47:22 +0300

    Small changes.

--- DirtX.Web/Controllers/ConverterController.cs
+++ DirtX.Web/Controllers/ConverterController.cs
@@ -12,8 +12,8 @@ namespace DirtX.Web.Controllers
         [HttpPost]
         public IActionResult Convert(int conversionType, double inputValue)
         {
-            double result = 0;
-            string resultMessage = "";
+            double result;
+            string resultMessage;
 
             switch (conversionType)
             {
@@ -21,20 +21,20 @@ namespace DirtX.Web.Controllers
                     result = inputValue * 2.54;
                     resultMessage = $"{inputValue} inches is equal to {result} centimeters.";
                     break;
-                case 2: 
+                case 2:
                     result = inputValue / 2.54;
                     resultMessage = $"{inputValue} centimeters is equal to {result} inches.";
                     break;
-                case 3: 
-                    result = inputValue * 0.453592; 
+                case 3:
+                    result = inputValue * 0.453592;
                     resultMessage = $"{inputValue} pounds is equal to {result} kilograms.";
                     break;
-                case 4: 
-                    result = inputValue / 0.453592; 
+                case 4:
+                    result = inputValue / 0.453592;
                     resultMessage = $"{inputValue} kilograms is equal to {result} pounds.";
                     break;
-                case 5: 
-                    result = inputValue * 1.60934; 
+                case 5:
+                    result = inputValue * 1.60934;
                     resultMessage = $"{inputValue} miles per hour is equal to {result} kilometers per hour.";
                     break;
                 case 6:

889e8ad75bd5edc3d3987fc35c5c001e7a1abdda
Vladislav <tsekov.vladislav@gmail.com>
2024-04-04 22:53:27 +0300

    Used DI for database reference in the TrailerService.

--- DirtX.Core/Services/TrailerService.cs
+++ DirtX.Core/Services/TrailerService.cs
@@ -1,10 +1,18 @@
 ﻿using DirtX.Core.Interfaces;
+using DirtX.Infrastructure.Data;
 using DirtX.Infrastructure.Data.Models.Trailers;
 
 namespace DirtX.Core.Services
 {
     public class TrailerService : ITrailerService
     {
+        private readonly ApplicationDbContext context;
+
+        public TrailerService(ApplicationDbContext _context)
+        {
+            context = _context;
+        }
+
         public Task<List<Trailer>> GetAllTrailersAsync()
         {
             throw new NotImplementedException();

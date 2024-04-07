410dd33d9f3f40cddf1cde2661addf4c09b4882d
Vladislav <tsekov.vladislav@gmail.com>
2024-04-07 18:22:09 +0300

    Implemented error logging and handling for the UsedController.

--- DirtX.Web/Controllers/UsedController.cs
+++ DirtX.Web/Controllers/UsedController.cs
@@ -1,6 +1,5 @@
 ﻿using DirtX.Core.Interfaces;
 using DirtX.Core.Validation;
-using DirtX.Infrastructure.Data;
 using DirtX.Infrastructure.Data.Models.Enums;
 using DirtX.Infrastructure.Data.Models.Motorcycles;
 using DirtX.Web.Models.Used;
@@ -11,110 +10,157 @@ namespace DirtX.Web.Controllers
 {
     public class UsedController : Controller
     {
-        //private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<UsedController> logger;
         private readonly IMotorcycleService motorcycleService;
 
-        public UsedController(ApplicationDbContext _context/*ILogger<HomeController> logger*/, IMotorcycleService _motorcycleService)
+        public UsedController(ILogger<UsedController> _logger, IMotorcycleService _motorcycleService)
         {
-            //_logger = logger;
+            logger = _logger;
             motorcycleService = _motorcycleService;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
-            List<UsedMotorcycle> usedMotorcycles = await motorcycleService.GetAllUsedMotorcyclesAsync();
-
-            List<UsedMotoViewModel> models = usedMotorcycles.Select(m => new UsedMotoViewModel
+            try
             {
-                Id = m.Id,
-                Make = m.Make.Title,
-                Model = m.Model.Title,
-                Displacement = m.Displacement.Volume,
-                Year = m.Year.ManufactureYear,
-                Image = m.Image,
-                Province = m.Province.ToString(),
-                Description = m.Description,
-                Price = m.Price,
-                Contact = m.Contact
-            }).ToList();
+                List<UsedMotorcycle> usedMotorcycles = await motorcycleService.GetAllUsedMotorcyclesAsync();
 
-            return View(models);
+                if (usedMotorcycles is null)
+                {
+                    logger.LogError("An error occurred in the Used/Index action. No used motorcycles found.");
+                    return NotFound();
+                }
+
+                List<UsedMotoViewModel> model = usedMotorcycles.Select(m => new UsedMotoViewModel
+                {
+                    Id = m.Id,
+                    Make = m.Make.Title,
+                    Model = m.Model.Title,
+                    Displacement = m.Displacement.Volume,
+                    Year = m.Year.ManufactureYear,
+                    Image = m.Image,
+                    Province = m.Province.ToString(),
+                    Description = m.Description,
+                    Price = m.Price,
+                    Contact = m.Contact
+                }).ToList();
+
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "An error occurred in the Used/Index action. Debug for more information.");
+                return View("Error");
+            }
         }
 
         [HttpGet]
-        public async Task<IActionResult> Details(int id) 
+        public async Task<IActionResult> Details(int id)
         {
-            //TODO - FIXE WHEN SERVICE IS IMPLEMENTED
-            //TODO - LIMIT IMAGE SIZE WHEN OPENING THE VIEW
-            //var motorcycleDetails = await usedService.GetMotorcycleDetailsById(id);
-
-            UsedMotorcycle motoDetails = await motorcycleService.GetUsedMotorcycleAsync(id);
-
-            if (motoDetails == null)
+            try
             {
-                return NotFound(); 
+                UsedMotorcycle motoDetails = await motorcycleService.GetUsedMotorcycleAsync(id);
+
+                if (motoDetails is null)
+                {
+                    logger.LogError($"An error occurred in the Used/Details action. Used motorcycle with ID '{id}' could not be found.");
+                    return NotFound();
+                }
+
+                UsedMotoViewModel model = new()
+                {
+                    Make = motoDetails.Make.Title,
+                    Model = motoDetails.Model.Title,
+                    Displacement = motoDetails.Displacement.Volume,
+                    Year = motoDetails.Year.ManufactureYear,
+                    Image = motoDetails.Image,
+                    Province = motoDetails.Province.ToString(),
+                    Description = motoDetails.Description,
+                    Price = motoDetails.Price,
+                    Contact = motoDetails.Contact
+                };
+
+                return View(model);
             }
-
-            if (!ModelState.IsValid)
+            catch (Exception ex)
             {
-                return NotFound();
+                logger.LogError(ex, "An error occurred in the Used/Details action. Debug for more information.");
+                return View("Error");
             }
-
-            var viewModel = new UsedMotoViewModel
-            {
-                Make = motoDetails.Make.Title,
-                Model = motoDetails.Model.Title,
-                Displacement = motoDetails.Displacement.Volume,
-                Year = motoDetails.Year.ManufactureYear,
-                Image = motoDetails.Image,
-                Province = motoDetails.Province.ToString(),
-                Description = motoDetails.Description,
-                Price = motoDetails.Price,
-                Contact = motoDetails.Contact
-            };
-
-            return View(viewModel);
         }
 
         [HttpGet]
         public async Task<IActionResult> Sell()
         {
-            var makes = new SellFormViewModel
+            try
             {
-                Makes = await motorcycleService.GetMotorcycleMake()
-            };
+                SellFormViewModel makes = new()
+                {
+                    Makes = await motorcycleService.GetMotorcycleMake()
+                };
 
-            return View(makes);
+                return View(makes);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "AJAX OPERATION ERROR: An error occurred while fetching motorcycle makes. Debug the Used/Sell action for more details.");
+                return View("Error");
+            }
         }
 
         [HttpGet]
         public async Task<IActionResult> GetModel(int makeId)
         {
-            List<SelectListItem> models = await motorcycleService.GetMotorcycleModel(makeId);
+            try
+            {
+                List<SelectListItem> models = await motorcycleService.GetMotorcycleModel(makeId);
 
-            return Json(models);
+                return Json(models);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "AJAX OPERATION ERROR: An error occurred while fetching motorcycle models. Debug the Used/GetModel action for more details.");
+                return View("Error");
+            }
         }
 
         [HttpGet]
         public async Task<IActionResult> GetDisplacement(int makeId, int modelId)
         {
-            List<SelectListItem> displacements = await motorcycleService.GetMotorcycleDisplacement(makeId, modelId);
+            try
+            {
+                List<SelectListItem> displacements = await motorcycleService.GetMotorcycleDisplacement(makeId, modelId);
 
-            return Json(displacements);
+                return Json(displacements);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "AJAX OPERATION ERROR: An error occurred while fetching motorcycle displacements. Debug the Used/GetDisplacement action for more details.");
+                return View("Error");
+            }
         }
 
         [HttpGet]
         public async Task<IActionResult> GetYear(int makeId, int modelId, int displacementId)
         {
-            var years = await motorcycleService.GetMotorcycleYears(makeId, modelId, displacementId);
+            try
+            {
+                List<SelectListItem> years = await motorcycleService.GetMotorcycleYears(makeId, modelId, displacementId);
 
-            return Json(years);
+                return Json(years);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "AJAX OPERATION ERROR: An error occurred while fetching motorcycle years. Debug the Used/GetYear action for more details.");
+                return View("Error");
+            }
         }
 
         [HttpPost]
         public async Task<IActionResult> Sell(SellFormViewModel model)
         {
-            if (model.ImageFile != null)
+            if (model.ImageFile is not null)
             {
                 model.Image = FormFileConverter.ConvertToByteArray(model.ImageFile);
             }
@@ -132,7 +178,7 @@ namespace DirtX.Web.Controllers
                 return View(model);
             }
 
-            var usedMotorcycle = new UsedMotorcycle
+            UsedMotorcycle usedMotorcycle = new() 
             {
                 MakeId = model.SelectedMake,
                 ModelId = model.SelectedModel,

96b2474b39fa2305b55239341601249541571a97
Vladislav <tsekov.vladislav@gmail.com>
2024-04-11 19:47:38 +0300

    Implemented UserService and interface for it.

---
+++ DirtX.Core/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using DirtX.Core.Models;
+using DirtX.Core.Models.Admin;
+
+namespace DirtX.Core.Interfaces
+{
+    public interface IUserService
+    {
+        public Task<IEnumerable<UserViewModel>> GetAllUsersAsync();
+        public Task EditProfileAsync(string userId, EditProfileViewModel model);
+        public Task DeleteUserAsync(string userId);
+        public Task PromoteUserToReseller(string userId);
+        public Task PromoteUserToAdmin(string userId);
+    }
+}

256aa7ccce12fb9e9997bd4aa52068795e33d6b7
Vladislav <tsekov.vladislav@gmail.com>
2024-04-11 19:50:30 +0300

    Implemented view model for the landing page of Admin area.

---
+++ DirtX.Core/Models/Admin/AdminIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DirtX.Core.Models.Admin
+{
+    public class AdminIndexViewModel
+    {
+        public IEnumerable<UserViewModel> Users { get; set; }
+
+        public IEnumerable<ProductViewModel> Products { get; set; }
+    }
+}

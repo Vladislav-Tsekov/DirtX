7bd24b59bdd045b0c09861a7453f1b0eb2ec5738
Vladislav <tsekov.vladislav@gmail.com>
2024-04-11 19:49:45 +0300

    Implemented a ProductViewModel for Admin area.

---
+++ DirtX.Core/Models/Admin/ProductViewModel.cs
@@ -0,0 +1,25 @@
+﻿using DirtX.Infrastructure.Data.Models.Enums;
+using DirtX.Infrastructure.Data.Models.Products;
+
+namespace DirtX.Core.Models.Admin
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+
+        //public ProductBrand Brand { get; set; }
+        public string Brand { get; set; }
+
+        //public ProductType Type { get; set; }
+
+        public string Type { get; set; }
+        //public ProductCategory Category { get; set; }
+
+        public string Category { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public int StockQuantity { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}

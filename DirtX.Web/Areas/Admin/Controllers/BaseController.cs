561eebd20d698793c4b15ccc12ccc0c6689b59ec
Vladislav <tsekov.vladislav@gmail.com>
2024-04-11 20:31:04 +0300

    Added a base controller for the Admin area, authorizing admins only.

---
+++ DirtX.Web/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DirtX.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class BaseController : Controller
+    {
+    }
+}

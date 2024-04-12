0f4486cf30d22f4865549fe4900827a7ab043a76
Vladislav <tsekov.vladislav@gmail.com>
2024-04-12 21:06:00 +0300

    Modified Endpoints/ControllerRoutes.

--- DirtX.Web/Program.cs
+++ DirtX.Web/Program.cs
@@ -87,9 +87,21 @@ app.UseSession();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "Admin",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+
+    endpoints.MapControllerRoute(
+   name: "default",
+   pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();

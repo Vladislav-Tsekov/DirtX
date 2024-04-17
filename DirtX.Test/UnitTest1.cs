8f482f5a64f901e36e83bb643cbb8cb7194a0ee1
Vladislav <tsekov.vladislav@gmail.com>
2024-04-17 19:00:57 +0300

    Added new project for test - nUnitTest one.

---
+++ DirtX.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace DirtX.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

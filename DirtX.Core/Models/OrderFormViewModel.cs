5645da7fbd7a037105a1f484d21ed36fce4bc6bd
Vladislav <tsekov.vladislav@gmail.com>
2024-04-17 17:24:21 +0300

    Additional implementations for Order - MVC and service.

---
+++ DirtX.Core/Models/OrderFormViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DirtX.Core.Models
+{
+
+
+    public class OrderFormViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "InvalidFirstName")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "InvalidFirstName")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "InvalidCountry")]
+        public string City { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "InvalidAddress")]
+        public string Address { get; set; }
+
+        public string UserId { get; set; }
+
+        public int CartId { get; set; }
+
+        public decimal TotalPrice { get; set; }
+    }
+}
---
+++ DirtX.Core/Services/OrderService.cs
@@ -0,0 +1,84 @@
+﻿using DirtX.Core.Interfaces;
+using DirtX.Core.Models;
+using DirtX.Infrastructure.Data;
+using DirtX.Infrastructure.Data.Models.Users;
+using DirtX.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DirtX.Core.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext context;
+
+        public OrderService(ApplicationDbContext _context)
+        {
+            context = _context;
+        }
+
+        public async Task<OrderFormViewModel> GetOrderDetailsAsync(int cartId)
+        {
+            OrderFormViewModel order = await context.Orders
+                .Where(o => o.CartId == cartId)
+                .Select(o => new OrderFormViewModel()
+                {
+                    Id = o.Id,
+                    FirstName = o.FirstName,
+                    LastName = o.LastName,
+                    City = o.City,
+                    Address = o.Address,
+                    CartId = o.CartId,
+                    UserId = o.UserId.ToString(),
+                    TotalPrice = o.TotalPrice
+                })
+                .FirstOrDefaultAsync();
+
+            return order;
+        }
+
+        public async Task CreateOrderAsync(OrderFormViewModel orderModel)
+        {
+            bool isValidUserId = Guid.TryParse(orderModel.UserId, out Guid validUserId);
+
+            if (isValidUserId)
+            {
+                Order order = new()
+                {
+                    FirstName = orderModel.FirstName,
+                    LastName = orderModel.LastName,
+                    City = orderModel.City,
+                    Address = orderModel.Address,
+                    CartId = orderModel.CartId,
+                    UserId = validUserId.ToString(),
+                    TotalPrice = orderModel.TotalPrice
+                };
+
+                await context.Orders.AddAsync(order);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<OrderFormViewModel> GetLastOrderListByUserIdAsync(string userId)
+        {
+            List<OrderFormViewModel> orderList = await context.Orders
+                .Where(o => o.UserId.ToString() == userId)
+                .Select(o => new OrderFormViewModel()
+                {
+                    Id = o.Id,
+                    FirstName = o.FirstName,
+                    LastName = o.LastName,
+                    City = o.City!,
+                    Address = o.Address,
+                    CartId = o.CartId,
+                    UserId = o.UserId.ToString(),
+                    TotalPrice = o.TotalPrice
+                })
+                .ToListAsync();
+
+            OrderFormViewModel lastOrder = orderList.LastOrDefault();
+
+            return (lastOrder);
+        }
+
+    }
+}
